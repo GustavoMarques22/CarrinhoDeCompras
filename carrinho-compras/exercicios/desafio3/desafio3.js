4c67d5125ebbe91af016ea8314ce7fd0ca61a42a
Gustavo <gustavo.marques@aluno.unievangelica.edu.br>
2025-05-01 22:18:35 -0300

    Add files via upload

---
+++ carrinho-compras/exercicios/desafio3/desafio3.js
@@ -0,0 +1,6 @@
+/*Tenha um elemento HTML na página (por exemplo, um parágrafo) e utilize JavaScript para modificar seu conteúdo usando a propriedade textContent.*/
+
+function modificarConteudo() {
+    const paragrafo = document.getElementById('meuParagrafo');
+    paragrafo.textContent = 'Novo texto modificado dinamicamente!';
+}
---
+++ carrinho-compras/exercicios/desafio5/desafio5.js
@@ -0,0 +1,6 @@
+/*Receba, no mínimo, duas sentenças como uma string e utilize split() para quebrá-la em frases menores com base em um ponto e vírgula como delimitador.*/
+
+const duasSentencas = "Criar o que não existe ainda deve ser a pretensão de todo sujeito que está vivo; A tarefa mais importante de uma pessoa que vem ao mundo é criar algo"
+const frasesSeparadas = duasSentencas.split(';');
+
+console.log(frasesSeparadas);
